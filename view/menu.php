1b3a0153085f96afa288c2b4b01b6a83297c9db7
Yunchae Choi <68932250+dngchoi337@users.noreply.github.com>
2022-06-01 16:47:58 +0900

    Update menu.php

--- view/menu.php
+++ view/menu.php
@@ -3,7 +3,7 @@
         <img src="../image/logo.jpg" />
     </a>
     <ul class="nav1">
-        <li><a href="#">공지사항</a></li>
+        <li><a href="../noticeboard/notice.php">공지사항</a></li>
         <li><a href="../enrollment/enrollment.php">수강신청</a>
             <ul class="nav2">
                 <li><a href="../enrollment/enrollment.php">수강신청하기</a></li>
@@ -11,9 +11,9 @@
                 <li><a href="../enrollment/enroll_check.php">과목 확인</a></li>
             </ul>
         </li>
-        <li><a href="#">시간표</a>
+        <li><a href="../timetable/timetable.php">시간표</a>
             <ul class="nav2">
-                <li><a href="#">시간표 및 강의계획</a></li>
+                <li><a href="../timetable/timetable.php">시간표 및 강의계획</a></li>
                 <li><a href="#">시간표 조합 생성</a></li>
             </ul>
         </li>
