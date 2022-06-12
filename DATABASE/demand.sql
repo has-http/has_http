f6e8420dba99c32827ad44f05fc81abbc8e4b74c
Aurora <aurora.kwon@gmail.com>
2022-06-12 23:27:03 +0900

    demand.sql boolean 컬럼 추가

--- DATABASE/demand.sql
+++ DATABASE/demand.sql
@@ -7,7 +7,8 @@ DROP TABLE IF EXISTS demand;
 CREATE TABLE demand (
     s_id        VARCHAR(30) NOT NULL,
     c_no        VARCHAR(30) NOT NULL,
-    t_no        INTEGER,     
+    t_no        INTEGER				,     
+    d_bool		BOOLEAN		NOT NULL DEFAULT 0,
     CONSTRAINT  PRIMARY KEY (s_id, c_no)
 );
 
