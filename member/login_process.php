6b51f8718842d75e12ad98e8d13a1ef316ffbfa2
Aurora <aurora.kwon@gmail.com>
2022-06-12 22:45:50 +0900

    update login_process.php

--- member/login_process.php
+++ member/login_process.php
@@ -4,14 +4,16 @@ session_start();
 $conn = mysql_connect();
 
 $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
+
+$_SESSION['isLogin'] = time();
+$_SESSION['user_id'] = $user_id;
+
 $temp = custom_query("SELECT * FROM student WHERE s_id = '".$user_id."'");
 $result = mysqli_fetch_array($temp);
-if (!$result) {
+if (!isset($result)) {
  $temp = custom_query("INSERT INTO student VALUES ('".$_SESSION['user_id'] ."', '_','_','0');"); // 학생 부가 데이터는 향후 추가
  $result = mysqli_fetch_array($temp);
 }
-$_SESSION['isLogin'] = time();
-$_SESSION['user_id'] = $user_id;
 header("Location: ../home/home2.php");
 exit;
 ?>
