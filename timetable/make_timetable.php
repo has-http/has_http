d4a59764c0d7d831356de8da8eee36d7f3d02ce8
ktwon3 <coreive35@gmail.com>
2022-06-13 00:50:18 +0900

    update make_timetable.php

--- timetable/make_timetable.php
+++ timetable/make_timetable.php
@@ -19,38 +19,6 @@
     </div>
     <div class="main">
         <h1>시간표 조합 생성</h1>
-<<<<<<< HEAD
-        <h2>본인이 선택한 과목을 바탕으로 가능한 시간표 조합을 모두 확인할 수 있습니다. 
-            <br> "저장" 버튼을 누르면 '수강신청' > '수강신청하기' 페이지에서 "장바구니" 기능을 사용할 수 있습니다.</h2>
-        <table border="2" bordercolor="#000" style="width:1000px;table-layout:fixed;word-break:break-all;" class="combination_table">
-            <thead>
-            <tr align="center" bgcolor="#298168">
-                <th>교시</th>
-                <th>월요일</th>
-                <th>화요일</th>
-                <th>수요일</th>
-                <th>목요일</th>
-                <th>금요일</th>
-            </tr>
-            </thead>
-            <tbody id="tbody"> 
-                <?php
-                require_once('../lib/enroll_func.php');
-                require_once('../lib/sub_list.php');
-                require_once('../lib/member_func.php');
-                $sub_array = get_demand_cno();
-                $fixed_array = get_demand_list($_SESSION['user_id']);
-                $result = get_all_case_client($sub_array);
-                $list = $result[0];
-                $tno_list = $result[1];
-                ?>
-                
-                <script>
-                    var block_list = <?php echo json_encode($list) ?>;
-                </script>
-            </tbody>
-        </table>
-=======
         <div class="timetable">
             <h2>본인이 선택한 과목을 바탕으로 가능한 시간표 조합을 모두 확인할 수 있습니다. 
                 <br> "저장" 버튼을 누르면 '수강신청' > '수강신청하기' 페이지에서 "장바구니" 기능을 사용할 수 있습니다.</h2>
@@ -82,7 +50,6 @@
                     </script>
                 </tbody>
             </table>
->>>>>>> 534dab7d8787b2493412553e3e73a0c92f1912cf
 
             <div class="table_controller">
                 <button class="arrow prev text" style="color:#fff;">이전</button>
