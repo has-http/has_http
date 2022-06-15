06ed1d28bd5196fffb08a7ec3f7492dae7a0ed3b
Yunchae Choi <68932250+dngchoi337@users.noreply.github.com>
2022-06-15 15:28:08 +0900

    Update demand.php

--- enrollment/demand.php
+++ enrollment/demand.php
@@ -1,9 +1,4 @@
 <?php 
     require('../view/demand_top.php');
-    require_once('../lib/demand_func.php');
-        $probability = get_probability_improved(get_teach_dict(), get_demand_tno());
-        if ($probability >= 0){
-            echo '<h1>' . round($probability, 3) * 100 . ' %</h1>';
-        }
     require('../view/demand_bottom.php');
 ?>
